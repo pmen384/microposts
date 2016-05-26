28162685f7bc4eb89e50e2ffaee06112ec3c05e1
takahiro sahashi <pmen384@gmail.com>
2016-05-26 13:20:40 +0000

    Lesson7 9.5 finished

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define do
+ActiveRecord::Schema.define(version: 20160526122032) do
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
@@ -21,8 +20,20 @@ ActiveRecord::Schema.define do
     t.datetime "updated_at", null: false
   end
 
-=======
->>>>>>> user-profile
+  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+  add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
@@ -33,6 +44,5 @@ ActiveRecord::Schema.define do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
 
 end
